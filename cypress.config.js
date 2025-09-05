bc52ec342d8f8a5da1baa01497ac21a47409a584
Justin Watanasiri <justin.watanasiri@gmail.com>
2025-09-04 23:07:50 -0400

    refactor: Update cypress.config.js for new folder structure

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  fixturesFolder: "cypress/fixtures",
 });
